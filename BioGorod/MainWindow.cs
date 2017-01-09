c2c83a03e435df36a3967b31c99f0ad221861acd
Andrey Gankov <gav@qsolution.ru>
2017-01-09 11:58:21 +0300

    Сделано минимальное наполнение главного окна.

--- BioGorod/MainWindow.cs
+++ BioGorod/MainWindow.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Gtk;
+using QSProjectsLib;
+using QSTDI;
+using QSSupportLib;
+using QSUpdater;
 
 public partial class MainWindow: Gtk.Window
 {
@@ -7,11 +11,81 @@ public partial class MainWindow: Gtk.Window
 		: base(Gtk.WindowType.Toplevel)
 	{
 		Build();
+		this.KeyReleaseEvent += ClipboardWorkaround.HandleKeyReleaseEvent;
+		TDIMain.MainNotebook = tdiMain;
+		this.KeyReleaseEvent += TDIMain.TDIHandleKeyReleaseEvent;
+		//Передаем лебл
+		QSMain.StatusBarLabel = labelStatus;
+		this.Title = MainSupport.GetTitle ();
+		QSMain.MakeNewStatusTargetForNlog ();
+
+		MainSupport.LoadBaseParameters ();
+
+		MainUpdater.RunCheckVersion (true, true, true);
+		QSMain.CheckServer (this); // Проверяем настройки сервера
+
+		if (QSMain.User.Login == "root") {
+			string Message = "Вы зашли в программу под администратором базы данных. У вас есть только возможность создавать других пользователей.";
+			MessageDialog md = new MessageDialog (this, DialogFlags.DestroyWithParent,
+				MessageType.Info, 
+				ButtonsType.Ok,
+				Message);
+			md.Run ();
+			md.Destroy ();
+			Users WinUser = new Users ();
+			WinUser.Show ();
+			WinUser.Run ();
+			WinUser.Destroy ();
+			return;
+		}
+
+		ActionUsers.Sensitive = QSMain.User.Admin;
+
 	}
 
 	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
 	{
-		Application.Quit();
-		a.RetVal = true;
+		if (tdiMain.CloseAllTabs ()) {
+			a.RetVal = false;
+			Application.Quit ();
+		} else {
+			a.RetVal = true;
+		}
 	}
+
+	protected void OnActionPasswordActivated(object sender, EventArgs e)
+	{
+		QSMain.User.ChangeUserPassword (this);
+	}
+		
+	protected void OnActionUsersActivated(object sender, EventArgs e)
+	{
+		Users winUsers = new Users ();
+		winUsers.Show ();
+		winUsers.Run ();
+		winUsers.Destroy ();
+	}
+
+	protected void OnQuitActionActivated(object sender, EventArgs e)
+	{
+		if (tdiMain.CloseAllTabs ()) {
+			Application.Quit ();
+		}
+	}
+
+	protected void OnAboutActionActivated(object sender, EventArgs e)
+	{
+		QSMain.RunAboutDialog ();
+	}
+
+	protected void OnActionCheckUpdateActivated(object sender, EventArgs e)
+	{
+		CheckUpdate.StartCheckUpdateThread (UpdaterFlags.ShowAnyway);
+	}
+
+	protected void OnActionHistoryActivated(object sender, EventArgs e)
+	{
+		QSMain.RunChangeLogDlg (this);
+	}
+
 }
--- BioGorod/gtk-gui/MainWindow.cs
+++ BioGorod/gtk-gui/MainWindow.cs
@@ -3,19 +3,127 @@
 
 public partial class MainWindow
 {
+	private global::Gtk.UIManager UIManager;
+	
+	private global::Gtk.Action Action;
+	
+	private global::Gtk.Action ActionPassword;
+	
+	private global::Gtk.Action ActionUsers;
+	
+	private global::Gtk.Action quitAction;
+	
+	private global::Gtk.Action Action2;
+	
+	private global::Gtk.Action Action3;
+	
+	private global::Gtk.Action ActionHistory;
+	
+	private global::Gtk.Action ActionCheckUpdate;
+	
+	private global::Gtk.Action aboutAction;
+	
+	private global::Gtk.VBox vbox1;
+	
+	private global::Gtk.MenuBar menubar1;
+	
+	private global::QSTDI.TdiNotebook tdiMain;
+	
+	private global::Gtk.Statusbar statusbar1;
+	
+	private global::Gtk.Label labelStatus;
+
 	protected virtual void Build ()
 	{
 		global::Stetic.Gui.Initialize (this);
 		// Widget MainWindow
+		this.UIManager = new global::Gtk.UIManager ();
+		global::Gtk.ActionGroup w1 = new global::Gtk.ActionGroup ("Default");
+		this.Action = new global::Gtk.Action ("Action", global::Mono.Unix.Catalog.GetString ("База"), null, null);
+		this.Action.ShortLabel = global::Mono.Unix.Catalog.GetString ("База");
+		w1.Add (this.Action, null);
+		this.ActionPassword = new global::Gtk.Action ("ActionPassword", global::Mono.Unix.Catalog.GetString ("Изменить пароль"), null, null);
+		this.ActionPassword.ShortLabel = global::Mono.Unix.Catalog.GetString ("Изменить пароль");
+		w1.Add (this.ActionPassword, null);
+		this.ActionUsers = new global::Gtk.Action ("ActionUsers", global::Mono.Unix.Catalog.GetString ("Пользователи"), null, "gtk-preferences");
+		this.ActionUsers.ShortLabel = global::Mono.Unix.Catalog.GetString ("Пользователи");
+		w1.Add (this.ActionUsers, null);
+		this.quitAction = new global::Gtk.Action ("quitAction", global::Mono.Unix.Catalog.GetString ("В_ыход"), null, "gtk-quit");
+		this.quitAction.ShortLabel = global::Mono.Unix.Catalog.GetString ("В_ыход");
+		w1.Add (this.quitAction, null);
+		this.Action2 = new global::Gtk.Action ("Action2", global::Mono.Unix.Catalog.GetString ("Справочники"), null, null);
+		this.Action2.ShortLabel = global::Mono.Unix.Catalog.GetString ("Справочники");
+		w1.Add (this.Action2, null);
+		this.Action3 = new global::Gtk.Action ("Action3", global::Mono.Unix.Catalog.GetString ("Справка"), null, null);
+		this.Action3.ShortLabel = global::Mono.Unix.Catalog.GetString ("Справка");
+		w1.Add (this.Action3, null);
+		this.ActionHistory = new global::Gtk.Action ("ActionHistory", global::Mono.Unix.Catalog.GetString ("История версий"), null, null);
+		this.ActionHistory.Sensitive = false;
+		this.ActionHistory.ShortLabel = global::Mono.Unix.Catalog.GetString ("История версий");
+		w1.Add (this.ActionHistory, null);
+		this.ActionCheckUpdate = new global::Gtk.Action ("ActionCheckUpdate", global::Mono.Unix.Catalog.GetString ("Проверить обновление..."), null, "gtk-go-down");
+		this.ActionCheckUpdate.ShortLabel = global::Mono.Unix.Catalog.GetString ("Проверить обновление...");
+		w1.Add (this.ActionCheckUpdate, null);
+		this.aboutAction = new global::Gtk.Action ("aboutAction", global::Mono.Unix.Catalog.GetString ("_О программе"), null, "gtk-about");
+		this.aboutAction.ShortLabel = global::Mono.Unix.Catalog.GetString ("_О программе");
+		w1.Add (this.aboutAction, null);
+		this.UIManager.InsertActionGroup (w1, 0);
+		this.AddAccelGroup (this.UIManager.AccelGroup);
 		this.Name = "MainWindow";
 		this.Title = global::Mono.Unix.Catalog.GetString ("MainWindow");
 		this.WindowPosition = ((global::Gtk.WindowPosition)(4));
+		// Container child MainWindow.Gtk.Container+ContainerChild
+		this.vbox1 = new global::Gtk.VBox ();
+		this.vbox1.Name = "vbox1";
+		this.vbox1.Spacing = 6;
+		// Container child vbox1.Gtk.Box+BoxChild
+		this.UIManager.AddUiFromString ("<ui><menubar name='menubar1'><menu name='Action' action='Action'><menuitem name='ActionPassword' action='ActionPassword'/><menuitem name='ActionUsers' action='ActionUsers'/><separator/><menuitem name='quitAction' action='quitAction'/></menu><menu name='Action2' action='Action2'/><menu name='Action3' action='Action3'><menuitem name='ActionHistory' action='ActionHistory'/><menuitem name='ActionCheckUpdate' action='ActionCheckUpdate'/><separator/><menuitem name='aboutAction' action='aboutAction'/></menu></menubar></ui>");
+		this.menubar1 = ((global::Gtk.MenuBar)(this.UIManager.GetWidget ("/menubar1")));
+		this.menubar1.Name = "menubar1";
+		this.vbox1.Add (this.menubar1);
+		global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.menubar1]));
+		w2.Position = 0;
+		w2.Expand = false;
+		w2.Fill = false;
+		// Container child vbox1.Gtk.Box+BoxChild
+		this.tdiMain = new global::QSTDI.TdiNotebook ();
+		this.tdiMain.Name = "tdiMain";
+		this.tdiMain.CurrentPage = 0;
+		this.tdiMain.ShowTabs = false;
+		this.vbox1.Add (this.tdiMain);
+		global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.tdiMain]));
+		w3.Position = 1;
+		// Container child vbox1.Gtk.Box+BoxChild
+		this.statusbar1 = new global::Gtk.Statusbar ();
+		this.statusbar1.Name = "statusbar1";
+		this.statusbar1.Spacing = 6;
+		// Container child statusbar1.Gtk.Box+BoxChild
+		this.labelStatus = new global::Gtk.Label ();
+		this.labelStatus.Name = "labelStatus";
+		this.labelStatus.LabelProp = global::Mono.Unix.Catalog.GetString ("label1");
+		this.statusbar1.Add (this.labelStatus);
+		global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(this.statusbar1 [this.labelStatus]));
+		w4.Position = 2;
+		w4.Expand = false;
+		w4.Fill = false;
+		this.vbox1.Add (this.statusbar1);
+		global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.statusbar1]));
+		w5.Position = 2;
+		w5.Expand = false;
+		w5.Fill = false;
+		this.Add (this.vbox1);
 		if ((this.Child != null)) {
 			this.Child.ShowAll ();
 		}
-		this.DefaultWidth = 400;
+		this.DefaultWidth = 568;
 		this.DefaultHeight = 300;
 		this.Show ();
 		this.DeleteEvent += new global::Gtk.DeleteEventHandler (this.OnDeleteEvent);
+		this.ActionPassword.Activated += new global::System.EventHandler (this.OnActionPasswordActivated);
+		this.ActionUsers.Activated += new global::System.EventHandler (this.OnActionUsersActivated);
+		this.quitAction.Activated += new global::System.EventHandler (this.OnQuitActionActivated);
+		this.ActionHistory.Activated += new global::System.EventHandler (this.OnActionHistoryActivated);
+		this.ActionCheckUpdate.Activated += new global::System.EventHandler (this.OnActionCheckUpdateActivated);
+		this.aboutAction.Activated += new global::System.EventHandler (this.OnAboutActionActivated);
 	}
 }
