9f6164a61f98c6f5a2aa0d063221fdf34429ddf1
Andrey Gankov <gav@qsolution.ru>
2017-02-07 10:09:36 +0300

    Исправлена ошибка сохранения расчетного счета.

--- BioGorod/CreateProjectParam.cs
+++ BioGorod/CreateProjectParam.cs
@@ -56,6 +56,15 @@ namespace BioGorod
 			};
 			OrmMain.ClassMappingList.AddRange (QSBanks.QSBanksMain.GetModuleMaping ());
 			OrmMain.ClassMappingList.AddRange (QSContactsMain.GetModuleMaping ());
+
+			//Настройка ParentReference
+			ParentReferenceConfig.AddActions (new ParentReferenceActions<Organization, QSBanks.Account> {
+				AddNewChild = (o, a) => o.AddAccount (a)
+			});
+			ParentReferenceConfig.AddActions (new ParentReferenceActions<Counterparty, QSBanks.Account> {
+				AddNewChild = (c, a) => c.AddAccount (a)
+			});
+
 		}
 	}
 }
